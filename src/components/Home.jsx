4f48f967bd1fad37ecf48f8a4512051bb10fd3b3
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-25 13:13:30 +0500

    fix: changes resolved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <h2>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iste, veritatis rerum est rem nostrum mollitia pariatur repudiandae tempora quos soluta, quaerat et at repellendus inventore veniam debitis aliquam aspernatur!</p>
+      
     </>
   )
 }

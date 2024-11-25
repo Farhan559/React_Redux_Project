90a4d4feb5608201bec06868a44846f65d5bd5a0
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-25 13:11:27 +0500

    Add project files

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error iste, veritatis rerum est rem nostrum mollitia pariatur repudiandae tempora quos soluta, quaerat et at repellendus inventore veniam debitis aliquam aspernatur!</p>
+    </>
+  )
+}
+
+export default Home
